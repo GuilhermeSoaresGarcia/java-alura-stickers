e9391a381120af3325cbeb9833798e7a9de251c8
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-03-29 17:32:41 -0300

    feat: adicionado parâmetro "rating" ao constructor, com getter opcional

--- src/Content.java
+++ src/Content.java
@@ -1,12 +1,17 @@
+import java.util.Optional;
+
 public class Content {
     // atributos da classe
     private final String title;
     private final String urlImage;
+    private final int rating;
+    
     
     // constructor
-    public Content(String title, String urlImage) {
+    public Content(String title, String urlImage, int rating) {
         this.title = title;
         this.urlImage = urlImage;
+        this.rating = rating;
     }
 
 
@@ -17,5 +22,10 @@ public class Content {
 
     public String getUrlImage() {
         return urlImage;
+    }    
+
+    // FONTE: https://levelup.gitconnected.com/java-optional-is-not-so-obvious-263d9559dd41
+    public Optional<Integer> getRating() {
+        return Optional.ofNullable(rating);
     }    
 }
